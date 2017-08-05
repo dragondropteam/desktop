47000f549e71bd8fc6c370cc4f77f67e897e4689
Unknown <unknown@example.com>
2017-08-04 17:04:28 -0700

    Fixes for windows makes it a frameless window

--- desktop/progress.js
+++ desktop/progress.js
@@ -5,7 +5,7 @@ exports.ProgressWindow  = class {
 
     constructor(msg) {
         this.modalWindow = null;
-        this.modalWindow = new BrowserWindow({width: 475, height: 175, parent: BrowserWindow.getFocusedWindow(), modal: true, show: false});
+        this.modalWindow = new BrowserWindow({width: 475, height: 175, frame: false,  parent: BrowserWindow.getFocusedWindow(), modal: true, show: false});
         this.modalWindow.loadURL(`file://${__dirname}/loading.html`);
 
         this.modalWindow.once('ready-to-show', () => {
