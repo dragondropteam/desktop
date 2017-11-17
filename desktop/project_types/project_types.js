6d0ebfae118f3328ece217d258a30e8666c04990
Luna Neumeister <luna.neumeister@proton.me>
2017-11-16 17:20:09 -0800

    Addded BaseProjectManager base class for each ProjectType to move all reusable code to a central location. Refactored Arduino projects to use this new method. Updated main_core to use  new methods

--- desktop/project_types/project_types.js
+++ desktop/project_types/project_types.js
@@ -5,17 +5,27 @@
 
 const Config = require('electron-store');
 const config = new Config();
+const fs = require('fs-extra');
+const {LoadedProject, Project} = require('../project/projects');
+const path = require('path');
 
-class ProjectType{
-  constructor(tag, display, requirePath, enabled){
-    this.tag = tag;
-    this.display = display;
-    this.requirePath = requirePath;
-    this.enabled = enabled;
-  }
+class ProjectType {
+    constructor(tag, display, requirePath, enabled) {
+        this.tag = tag;
+        this.display = display;
+        this.requirePath = requirePath;
+        this.enabled = enabled;
+    }
 }
 
-const defaultValue = [new ProjectType('wink', 'PlumGeek Robotics Wink', './project_types/wink_robot', true), new ProjectType('arduino', 'Arduino', './project_types/arduino', true), new ProjectType('ringo', 'PlumGeek Robotics Ringo', './project_types/ringo_robot', false), new ProjectType('block_javascript', 'JavaScript Blocks', './project_types/basic_javascript', true), new ProjectType('visual_phaser', 'Visual Phaser Game Engine', './project_types/visual_phaser', true), new ProjectType('text_phaser', 'Phaser Game Engine', './project_types/text_phaser', true)];
+const defaultValue = [
+    new ProjectType('wink', 'PlumGeek Robotics Wink', './project_types/wink_robot', true),
+    new ProjectType('arduino', 'Arduino', './project_types/arduino', true),
+    new ProjectType('ringo', 'PlumGeek Robotics Ringo', './project_types/ringo_robot', false),
+    new ProjectType('block_javascript', 'JavaScript Blocks', './project_types/basic_javascript', true),
+    new ProjectType('visual_phaser', 'Visual Phaser Game Engine', './project_types/visual_phaser', true),
+    new ProjectType('text_phaser', 'Phaser Game Engine', './project_types/text_phaser', true)];
+
 const projectTypesKey = 'PROJECT_TYPES';
 
 exports.ProjectType = ProjectType;
@@ -25,19 +35,148 @@ exports.ProjectType = ProjectType;
  * modifications have been made to the settings
  * @returns {*[]} An array of ProjectType
  */
-function getProjectTypes(){
-  var projectTypes = config.get(projectTypesKey);
-  return projectTypes || defaultValue;
+function getProjectTypes() {
+    var projectTypes = config.get(projectTypesKey);
+    return projectTypes || defaultValue;
 }
 
 exports.getProjectTypes = getProjectTypes;
 
-exports.getRequirePath = function(tag){
-  var requirePath = null;
-  getProjectTypes().forEach((project)=>{
-    if(project.tag == tag){
-      requirePath = project.requirePath;
+exports.getRequirePath = function (tag) {
+    var requirePath = null;
+    getProjectTypes().forEach((project) => {
+        if (project.tag == tag) {
+            requirePath = project.requirePath;
+        }
+    });
+    return requirePath;
+};
+
+
+exports.BaseProjectManager = class BaseProjectManager {
+    constructor(buildNumber, type, staticRoot) {
+        this.buildNumber = buildNumber;
+        this.type = type;
+        this.staticRoot = staticRoot;
     }
-  });
-  return requirePath;
+
+    /**
+     * Create a new project
+     * @param name The name of the project
+     * @param filePath The path to create the new project directory
+     * @param version The version of DragonDrop
+     * @return {LoadedProject} A LoadedProject representing the newly created project.
+     */
+    createNewProject(name, filePath, version) {
+        console.log(`Creating project ${name} at ${filePath} with version ${version}`);
+        try {
+            this.createProjectDir(name, filePath);
+            this.copyBaseFiles(name, filePath);
+            let project = new Project(name, version, this.type, this.createMeta());
+            fs.writeJsonSync(path.join(filePath, `${name}.digiblocks`), project);
+            return new LoadedProject(project, filePath);
+        } catch (e) {
+            console.error(e);
+            return null;
+        }
+    }
+
+    /**
+     * Ensure the directories we need exist creating them if not
+     * @private
+     * @param name The name of the project
+     * @param filePath path to the directory to create the project in
+     */
+    createProjectDir(name, filePath) {
+        if (!path.isAbsolute(filePath)) {
+            throw new Error('filePath must be an absolute path');
+        }
+
+        fs.ensureDirSync(filePath);
+        fs.ensureDirSync(path.join(filePath, name));
+    }
+
+    /**
+     * Copy and starting files to created directory if any defaults to doing nothing.
+     * @param name
+     * @param filePath
+     */
+    copyBaseFiles(name, filePath) {
+        /*NO-OP*/
+    }
+
+
+    /**
+     * Create any metadata needed for this project
+     * @abstract
+     */
+    createMeta() {
+        throw new Error('Invalid operation calling abstract BaseProject.createMeta');
+    }
+
+    /**
+     * Loads the .digiblocks (JSON) file and generates a LoadedProject to add helper methods and the load location for
+     * the project
+     * @param filePath The file path to a .digiblocks file to load
+     *
+     */
+    loadProject(filePath) {
+        let project = fs.readJsonSync(filePath);
+        let loadedProject = new LoadedProject(project, path.dirname(filePath));
+
+        if ((project.meta && project.meta.version < this.buildNumber) || (!project.meta) || (!project.type)) {
+            this.migrate(loadedProject)
+        }
+
+        return loadedProject;
+    }
+
+    /**
+     * Make any changes to the projects metadata to account for version changes
+     * @abstract
+     * @param loadedProject The project to migrate to a new versionn
+     * to version
+     */
+    migrate(loadedProject) {
+        throw new Error('Invalid Operation calling abstract BaseProject.migrate');
+    }
+
+    /**
+     * Add project type specific menu items
+     * @abstract
+     * @param menu The menu hash to add new items to
+     * @param project Currently loaded project
+     * @param success Callback when done creating the menu
+     * @param failure Callback if the menu can fail to be created
+     * @param refresh Callback to reload the menu if changes need to be applied at runtime
+     */
+    mutateMenu(menu, project, success, failure, refresh) {
+        throw new Error('Invalid Operation calling abstract BaseProject.mutateMenu');
+    }
+
+    /**
+     * Saves the project out to disk
+     * @param project The project to save to disk
+     */
+    saveProject(project) {
+        fs.outputJsonSync(project.getProjectPath(), project.loadedProject);
+    }
+
+    /**
+     * Display the loadedProject in the given window
+     * @param window The window that was created in main_core.js
+     * @param {Boolean} debug true in debug environment false in production
+     * @param {LoadedProject} project The LoadedProject to display in the opened window
+     */
+    displayProject(window, debug, project) {
+        //index_dev.html will use uncompressed versions of the blockly library index.html will use the compressed versions
+        //that are used in production
+        let index = debug ? 'index_dev.html' : 'index.html';
+        console.log(`displaying ${`file://${__dirname}/static/${index}`} in ${debug ? 'debug' : 'production'}`);
+
+        window.loadURL(`file://${this.staticRoot}/${index}`);
+        window.webContents.on('did-finish-load', () => {
+            window.send('set_project', project);
+        })
+    }
 };
