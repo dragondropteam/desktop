37f3186bacbe24aeb46c9021fcfd7ce3817115ce
Luna Neumeister <luna.neumeister@proton.me>
2017-12-05 13:28:07 -0800

    Moved over to using icons for Exploring Wink project type rearranged toolbar to get close to confluence

---
+++ desktop/icon_blocks/generators/c/wink_icon.js
@@ -0,0 +1,26 @@
+Blockly.C['seteyesred_icon'] = Blockly.C['seteyesred'];
+Blockly.C['seteyesgreen_icon'] = Blockly.C['seteyesgreen'];
+Blockly.C['seteyesblue_icon'] = Blockly.C['seteyesblue'];
+Blockly.C['seteyespink_icon'] = Blockly.C['seteyespink'];
+Blockly.C['seteyesyellow_icon'] = Blockly.C['seteyesyellow'];
+Blockly.C['seteyescyan_icon'] = Blockly.C['seteyescyan'];
+Blockly.C['seteyesorange_icon'] = Blockly.C['seteyesorange'];
+Blockly.C['seteyeswhite_icon'] = Blockly.C['seteyeswhite'];
+
+Blockly.C['setleftred_icon'] = Blockly.C['setleftred'];
+Blockly.C['setleftgreen_icon'] = Blockly.C['setleftgreen'];
+Blockly.C['setleftblue_icon'] = Blockly.C['setleftblue'];
+Blockly.C['setleftpink_icon'] = Blockly.C['setleftpink'];
+Blockly.C['setleftyellow_icon'] = Blockly.C['setleftyellow'];
+Blockly.C['setleftcyan_icon'] = Blockly.C['setleftcyan'];
+Blockly.C['setleftorange_icon'] = Blockly.C['setleftorange'];
+Blockly.C['setleftwhite_icon'] = Blockly.C['setleftwhite'];
+
+Blockly.C['setrightred_icon'] = Blockly.C['setrightred'];
+Blockly.C['setrightgreen_icon'] = Blockly.C['setrightgreen'];
+Blockly.C['setrightblue_icon'] = Blockly.C['setrightblue'];
+Blockly.C['setrightpink_icon'] = Blockly.C['setrightpink'];
+Blockly.C['setrightyellow_icon'] = Blockly.C['setrightyellow'];
+Blockly.C['setrightcyan_icon'] = Blockly.C['setrightcyan'];
+Blockly.C['setrightorange_icon'] = Blockly.C['setrightorange'];
+Blockly.C['setrightwhite_icon'] = Blockly.C['setrightwhite'];
---
+++ desktop/project_types/exploring_wink_robot/index.js
@@ -0,0 +1,118 @@
+/**
+ * All content copyright DigiPen Institute of Technology 2016
+ *
+ * Created by lukepowell on 11/8/16.
+ */
+require('../../project/projects');
+
+/** Version of the wink library separate from the overall application
+ *
+ * @type {number}
+ */
+const BUILD_NUMBER = 3;
+const PROJECT_TYPE = 'exploring_wink';
+const fs = require('fs-extra');
+const path = require('path');
+const filesystem = require(`filesystem`);
+const arduino = require('../../arduino_core/arduino_core');
+const electron = require('electron');
+const {BrowserWindow} = require('electron');
+const {BaseProjectManager} = require('../project_types');
+
+class ExploringWinkRobotProjectManager extends BaseProjectManager {
+    constructor() {
+        super(BUILD_NUMBER, PROJECT_TYPE, `${__dirname}/static`);
+    }
+
+    copyBaseFiles(name, filePath) {
+        fs.createFileSync(path.join(filePath, name, `${name}.ino`));
+        fs.copySync(filesystem.getFilePath('project_types/exploring_wink_robot/core_files/Wink_BaseSketch_Rev01_03'), path.join(filePath, name));
+    }
+
+    createMeta() {
+        return {
+            'version': BUILD_NUMBER,
+            'board': 'Arduino Fio'
+        };
+    }
+
+    migrate(loadedProject) {
+        console.log(`Migrating project from ${loadedProject.loadedProject.meta.version} to ${BUILD_NUMBER}`);
+
+        this.migrateMetaAndProjectType(loadedProject);
+
+        if (loadedProject.loadedProject.meta.version === 1) {
+            console.log('1 => 2\n Update meta to include board');
+            loadedProject.loadedProject.meta.board = 'Arduino Fio';
+            loadedProject.loadedProject.meta.version++;
+        }
+
+        //For now remove any .h file and .ino file and replace with .hpp and the .ino that reference the .hpp files
+        if (loadedProject.loadedProject.meta.version === 2) {
+            console.log('2 => 3\n  - Update .h to .hpp');
+
+            try {
+                fs.removeSync(loadedProject.getFileInProjectDir('FunStuff.ino'));
+                console.log('FunStuff.ino deleted');
+                fs.removeSync(loadedProject.getFileInProjectDir('FunStuff.h'));
+                console.log('FunStuff.h deleted');
+                fs.removeSync(loadedProject.getFileInProjectDir('WinkHardware.ino'));
+                console.log('WinkHardware.ino deleted');
+                fs.removeSync(loadedProject.getFileInProjectDir('WinkHardware.h'));
+                console.log('WinkHardware.h deleted');
+                fs.copySync(filesystem.getFilePath('project_types/wink_robot/core_files/Wink_BaseSketch_Rev01_03'), loadedProject.getProjectDir());
+                console.log('.h updated to .hpp');
+                loadedProject.loadedProject.meta.version++;
+            } catch (err) {
+                return console.error(err);
+            }
+        }
+
+        this.saveProject(loadedProject);
+    }
+
+    mutateMenu(menu, project, success, failure, refresh) {
+        arduino.addCoreArduinoMenuOptions(menu, project, completedProject, completedVerify, 'Upload Program to Wink Bot', 'Uploading Program to Wink Bot');
+        arduino.addPort(menu, project, success, failure, refresh, this.saveProject);
+    }
+}
+
+module.exports = new ExploringWinkRobotProjectManager();
+
+function completedProject(code, output) {
+    if (code === 0) {
+        //success
+        arduino.showUploadSuccess('Wink Bot');
+
+        electron.dialog.showMessageBox({
+            type: "info",
+            message: `Program Uploaded To Your Wink Bot`,
+            buttons: ["OK"]
+        });
+    } else {
+        arduino.showUploadFailure('Wink Bot');
+
+        electron.dialog.showMessageBox(BrowserWindow.getFocusedWindow(), {
+            type: 'error',
+            title: 'Dragon Drop Error',
+            message: 'Error Uploading To Wink Bot\nSee Arduino IDE for details.'
+        });
+    }
+}
+
+function completedVerify(code, output) {
+    if (code === 0) {
+        //success
+        arduino.showVerifySuccess('Wink Bot');
+
+        electron.dialog.showMessageBox({type: "info", message: `Program Verified`, buttons: ["OK"]});
+    } else {
+        arduino.showVerifyFailure('Wink Bot');
+
+        electron.dialog.showMessageBox(BrowserWindow.getFocusedWindow(), {
+            type: 'error',
+            title: 'Dragon Drop Error',
+            message: 'Error Uploading To Wink Bot\nSee Arduino IDE for details.'
+        });
+    }
+}
