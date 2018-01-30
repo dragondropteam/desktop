97a160f7e1ae5f66258d6d0aaf889f0b6c4c336b
Luna Neumeister <luna.neumeister@proton.me>
2018-01-30 14:43:21 -0800

    Fixes to generation of 0.0f better regexp matching Blocks generated for Serial and Advanced IO that are possible without a byte/char input type

--- desktop/blocks/arduino_serial.js
+++ desktop/blocks/arduino_serial.js
@@ -1,6 +1,6 @@
 //All content copyright DigiPen Institute of Technology 2016
 Blockly.Blocks['serial_read'] = {
-    init: function() {
+    init: function () {
         this.appendDummyInput()
             .appendField(Blockly.Msg.SERIAL_READ_FIELD_1);
         this.setOutput(true, null);
@@ -11,7 +11,7 @@ Blockly.Blocks['serial_read'] = {
 };
 
 Blockly.Blocks['serial_print_line'] = {
-    init: function() {
+    init: function () {
         this.appendValueInput("OUTPUT")
             .setCheck(null)
             .appendField(Blockly.Msg.SERIAL_PRINT_LINE_FIELD_1);
@@ -24,7 +24,7 @@ Blockly.Blocks['serial_print_line'] = {
 };
 
 Blockly.Blocks['serial_print'] = {
-    init: function() {
+    init: function () {
         this.appendValueInput("OUTPUT")
             .setCheck(null)
             .appendField(Blockly.Msg.SERIAL_PRINT_FIELD_1);
@@ -37,7 +37,7 @@ Blockly.Blocks['serial_print'] = {
 };
 
 Blockly.Blocks['serial_end'] = {
-    init: function() {
+    init: function () {
         this.appendDummyInput()
             .appendField(Blockly.Msg.SERIAL_END_FIELD_1);
         this.setPreviousStatement(true, null);
@@ -49,7 +49,7 @@ Blockly.Blocks['serial_end'] = {
 };
 
 Blockly.Blocks['serial_begin'] = {
-    init: function() {
+    init: function () {
         this.appendValueInput("SPEED")
             .setCheck(null)
             .appendField(Blockly.Msg.SERIAL_BEGIN_FIELD_1);
@@ -62,3 +62,148 @@ Blockly.Blocks['serial_begin'] = {
         this.setHelpUrl(Blockly.Msg.SERIAL_BEGIN_HELP_URL);
     }
 };
+
+Blockly.Blocks['serial_available'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_AVAILABLE);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_AVAILABLE_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_AVAILABLE_HELP_URL);
+        this.setOutput(true, 'Number');
+        this.setColour(230);
+    }
+};
+
+Blockly.Blocks['serial_available_write'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_AVAILABLE_FOR_WRITE);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_AVAILABLE_FOR_WRITE_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_AVAILABLE_FOR_WRITE_HELP_URL);
+        this.setOutput(true, 'Number');
+        this.setColour(230);
+    }
+};
+
+Blockly.Blocks['serial_find'] = {
+    init: function () {
+        this.appendValueInput('TARGET')
+            .setCheck('String')
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_FIND);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_FIND_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_FIND_HELP_URL);
+        this.setColour(230);
+        this.setOutput(true, 'Boolean');
+    }
+};
+
+Blockly.Blocks['serial_find_until'] = {
+    init: function () {
+        this.appendValueInput('TARGET')
+            .setCheck('String')
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_FIND_UNTIL);
+        this.appendValueInput('TERMINAL')
+            .setCheck('String')
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_UNTIL);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_FIND_UNTIL_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_FIND_UNTIL_HELP_URL);
+        this.setColour(230);
+        this.setOutput(true, 'Boolean');
+    }
+};
+
+Blockly.Blocks['serial_flush'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_FLUSH);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_FLUSH_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_FLUSH_HELP_URL);
+        this.setNextStatement(true);
+        this.setPreviousStatement(true)
+        this.setColour(230);
+    }
+};
+
+Blockly.Blocks['serial_parse_float'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_PARSE_FLOAT);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_PARSE_FLOAT_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_PARSE_FLOAT_HELP_URL);
+        this.setOutput(true, 'Number');
+        this.setColour(230);
+    }
+};
+
+Blockly.Blocks['serial_parse_int'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_PARSE_INT);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_PARSE_INT_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_PARSE_INT_HELP_URL);
+        this.setOutput(true, 'Number');
+        this.setColour(230);
+    }
+};
+
+Blockly.Blocks['serial_peek'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_PEEK);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_PEEK_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_PEEK_HELP_URL);
+        this.setOutput(true, 'Number');
+        this.setColour(230);
+    }
+};
+Blockly.Blocks['serial_read_bytes'] = {
+    init: function() {
+        this.appendValueInput('BUFFER')
+            .setCheck('ArrayINT')
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_READ_BYTES);
+        this.appendValueInput('LENGTH')
+            .setCheck('Number')
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_READ_BYTES_UP_TO);
+        this.setOutput(true);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_READ_BYTES_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_READ_BYTES_HELP_URL);
+        this.setOutput(true, 'Number');
+        this.setColour(230);
+    }
+};
+
+Blockly.Blocks['serial_read_bytes_until'] = {
+    init: function() {
+        this.appendValueInput('BUFFER')
+            .setCheck('ArrayINT')
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_READ_BYTES);
+        this.appendValueInput('LENGTH')
+            .setCheck('Number')
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_READ_BYTES_UP_TO);
+        this.appendValueInput('TERMINAL')
+            .setCheck('Number')
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_READ_BYTES_UNTIL_TERMINAL)
+        this.setOutput(true);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_READ_BYTES_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_READ_BYTES_HELP_URL);
+        this.setOutput(true, 'Number');
+        this.setColour(230);
+    }
+};
+
+Blockly.Blocks['serial_set_timeout'] = {
+    init: function() {
+        this.appendValueInput('MILLISECONDS')
+            .setCheck('Number')
+            .appendField(Blockly.Msg.ARDUINO_SERIAL_SET_TIMEOUT);
+        this.appendDummyInput()
+            .appendField(Blockly.Msg.MILLISECONDS);
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip(Blockly.Msg.ARDUINO_SERIAL_SET_TIMEOUT_TOOLTIP);
+        this.setHelpUrl(Blockly.Msg.ARDUINO_SERIAL_SET_TIMEOUT);
+        this.setColour(230);
+        this.setInputsInline(true);
+    }
+};
+
