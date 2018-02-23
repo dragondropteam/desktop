a1df087e660c04de20b10f76bc67687373aa988d
Luna Neumeister <luna.neumeister@proton.me>
2018-02-23 13:32:09 -0800

    Resolves DRAG-403

--- desktop/icon_blocks/generators/javascript/procedure_icon.js
+++ desktop/icon_blocks/generators/javascript/procedure_icon.js
@@ -1,2 +1,2 @@
-Blockly.JavaScript['procedures_defnoreturn_icon'] = Blockly.JavaScript['procedures_defnoreturn_icon'];
-Blockly.JavaScript['procedures_defreturn_icon'] = Blockly.JavaScript['procedures_defreturn_icon'];
+Blockly.JavaScript['procedures_defnoreturn_icon'] = Blockly.JavaScript['procedures_defnoreturn'];
+Blockly.JavaScript['procedures_defreturn_icon'] = Blockly.JavaScript['procedures_defreturn'];
