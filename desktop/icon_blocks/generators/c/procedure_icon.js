5961e78a617b74f9e772e7cc936a9f7b30e22a52
Luke Powell <raistlinpowell@gmail.com>
2018-03-16 14:12:11 -0700

    Fixes to wink using JavaScript libraries

---
+++ desktop/icon_blocks/generators/c/procedure_icon.js
@@ -0,0 +1,2 @@
+Blockly.C['procedures_defnoreturn_icon'] = Blockly.C['procedures_defnoreturn'];
+Blockly.C['procedures_defreturn_icon'] = Blockly.C['procedures_defreturn'];
--- desktop/icon_blocks/generators/javascript/loops_icon.js
+++ desktop/icon_blocks/generators/javascript/loops_icon.js
@@ -1,6 +1,6 @@
-Blockly.JavaScript['controls_repeat_ext_icon'] = Blockly.JavaScript['controls_repeat_ext'];
-Blockly.JavaScript['controls_repeat_icon'] = Blockly.JavaScript['controls_repeat_icon'];
-Blockly.JavaScript['controls_whileUntil_icon'] = Blockly.JavaScript['controls_whileUntil'];
-Blockly.JavaScript['controls_doWhile_icon'] = Blockly.JavaScript['controls_doWhile'];
-Blockly.JavaScript['controls_for_icon'] = Blockly.JavaScript['controls_for'];
-Blockly.JavaScript['controls_forEach_icon'] = Blockly.JavaScript['controls_forEach'];
+Blockly.C['controls_repeat_ext_icon'] = Blockly.C['controls_repeat_ext'];
+Blockly.C['controls_repeat_icon'] = Blockly.C['controls_repeat_icon'];
+Blockly.C['controls_whileUntil_icon'] = Blockly.C['controls_whileUntil'];
+Blockly.C['controls_doWhile_icon'] = Blockly.C['controls_doWhile'];
+Blockly.C['controls_for_icon'] = Blockly.C['controls_for'];
+Blockly.C['controls_forEach_icon'] = Blockly.C['controls_forEach'];
