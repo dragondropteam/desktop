2db776dfb47bb006955c307b8937da75f3d92c56
Luke Powell <raistlinpowell@gmail.com>
2018-03-16 15:26:46 -0700

    Fix loop blocks
    loop_icons generator added to C
    loop_icons generator in JavaScript reverted to JavaScript from C
    JavaScript APIS removed from wink dev environment

---
+++ desktop/icon_blocks/generators/c/loops_icon.js
@@ -0,0 +1,6 @@
+Blockly.C['controls_repeat_ext_icon'] = Blockly.C['controls_repeat_ext'];
+Blockly.C['controls_repeat_icon'] = Blockly.C['controls_repeat_icon'];
+Blockly.C['controls_whileUntil_icon'] = Blockly.C['controls_whileUntil'];
+Blockly.C['controls_doWhile_icon'] = Blockly.C['controls_doWhile'];
+Blockly.C['controls_for_icon'] = Blockly.C['controls_for'];
+Blockly.C['controls_forEach_icon'] = Blockly.C['controls_forEach'];
