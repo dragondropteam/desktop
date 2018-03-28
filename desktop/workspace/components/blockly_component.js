64178d90ce2844d2fe45667cbfba712979d855b6
Luna Neumeister <luna.neumeister@proton.me>
2018-03-28 16:31:17 -0700

    Components => Module

---
+++ desktop/workspace/components/blockly_component.js
@@ -0,0 +1,203 @@
+const {BaseComponent, TYPE_COMPONENT, TIMEOUT} = require('./component');
+const assert = require('assert');
+const Rx = require('rxjs/Rx');
+const BLOCKLY_DIV_ID = 'blocklyDiv';
+const BLOCKLY_AREA_ID = 'blocklyArea';
+
+class BlocklyComponent extends BaseComponent {
+
+    static get ID() {
+      return 'blocklyComponent';
+    }
+
+    static get TITLE() {
+        return 'Blockly';
+    }
+
+    static registerComponent(workspace, ipc) {
+        workspace.layout.registerComponent(BlocklyComponent.ID, BlocklyComponent);
+        ipc.on('show_blockly', workspace.addComponentIfMissing.bind(workspace, BlocklyComponent.ID, BlocklyComponent.TITLE))
+    }
+
+    /**
+     * Generate an object representing the configuration of a BlocklyComponent for GoldenLayout
+     * @param blocklyConfig {@link https://developers.google.com/blockly/guides/get-started/web#configuration}
+     * @param workspaceToCode A function to map a workspace to {@see GeneratedCode}
+     * @return {*} An Item Configuration for GoldenLayout {@link http://golden-layout.com/docs/ItemConfig.html}
+     */
+    static generateContent(blocklyConfig, workspaceToCode) {
+        assert(blocklyConfig != null);
+
+        return {
+            type: TYPE_COMPONENT,
+            title: BlocklyComponent.TITLE,
+            componentName: BlocklyComponent.ID,
+            componentState: {
+                label: BlocklyComponent.ID,
+                blocklyConfig: blocklyConfig,
+                workspaceToCode: workspaceToCode
+            }
+        }
+    }
+
+    static generateWorkspaceMappingFunction(workspaceToCode) {
+        return function (workspace) {
+            const code = workspaceToCode(workspace);
+            const dom = Blockly.Xml.workspaceToDom(workspace);
+            const xml = Blockly.Xml.domToText(dom);
+            return {code: code, xml: xml};
+        }
+    }
+
+    //TODO: This will need to take into account the users sound preference at some point
+    static getDefaultBlocklyConfig(toolboxSource) {
+        return {
+            comments: true,
+            disable: true,
+            collapse: true,
+            grid: {
+                spacing: 25,
+                length: 3,
+                colour: '#ccc',
+                snap: true
+            },
+            maxBlocks: Infinity,
+            media: '../../../media/',
+            readOnly: false,
+            rtl: false,
+            scrollbars: true,
+            toolbox: toolboxSource,
+            zoom: {
+                controls: true,
+                wheel: true,
+                startScale: 1.0,
+                maxScale: 4,
+                minScale: .25,
+                scaleSpeed: 1.1
+            }
+        };
+    }
+
+    constructor(container, componentState) {
+        super(componentState);
+
+        this.blocklyConfig = componentState.blocklyConfig;
+        this.workspaceToCode = componentState.workspaceToCode;
+        this.codeObservable_ = new Rx.Subject();
+
+        this.blocklyContainer = container;
+        this.blocklyContainer.getElement().html('<div id="blocklyArea"><div id="blocklyDiv" style="position: absolute"></div></div>');
+
+        this.blocklyContainer.on('open', () => {
+            this.setupDOM();
+        });
+
+        this.blocklyContainer.on('resize', () => {
+            if (!this.workspace) {
+                return;
+            }
+            this.resize();
+        });
+
+        this.blocklyContainer.on('destroy', () => {
+            this.blocklyContainer = null;
+            this.blocklyArea = null;
+        });
+    }
+
+    resize() {
+        // Compute the absolute coordinates and dimensions of blocklyArea.
+        let element = this.blocklyArea;
+
+        if (!element) {
+            setTimeout(this.resize.bind(this), TIMEOUT);
+            return;
+        }
+
+        let x = 0;
+        let y = 0;
+        do {
+            x += element.offsetLeft;
+            y += element.offsetTop;
+            element = element.offsetParent;
+        } while (element);
+
+        // Position blocklyDiv over blocklyArea.
+        this.blocklyDiv.style.left = `${x}px`;
+        this.blocklyDiv.style.top = `${y}px`;
+        this.blocklyDiv.style.width = `${blocklyArea.offsetWidth}px`;
+        this.blocklyDiv.style.height = `${blocklyArea.offsetHeight}px`;
+        Blockly.svgResize(this.workspace);
+    }
+
+    setupDOM() {
+        this.blocklyArea = document.getElementById(BLOCKLY_AREA_ID);
+        if (!this.blocklyArea) {
+            setTimeout(this.setupDOM.bind(this), TIMEOUT);
+            return false;
+        }
+
+        this.blocklyArea = document.getElementById(BLOCKLY_AREA_ID);
+        this.blocklyDiv = document.getElementById(BLOCKLY_DIV_ID);
+
+        this.workspace = Blockly.inject(BLOCKLY_DIV_ID, this.blocklyConfig);
+        this.workspace.addChangeListener(this.blocklyUpdate.bind(this));
+
+        this.resize();
+
+        return true;
+    }
+
+    getWorkspace() {
+        return this.workspace;
+    }
+
+    get codeObservable() {
+        return this.codeObservable_
+            .map(this.workspaceToCode);
+    }
+
+    blocklyUpdate(event) {
+        try {
+            /**
+             * All events in Blockly excluding Blockly.Events.UI are used for meaningful changes, Blockly.Events.UI
+             * is for context menu, toolbox and the like opening no reason to spin off a disk operation
+             */
+            if (event.type !== Blockly.Events.UI) {
+                const block = this.workspace.getBlockById(event.blockId);
+                if (block && block.onchange) {
+                    block.onchange(event);
+                }
+
+
+                this.codeObservable_.next(this.workspace);
+            }
+        } catch (err) {
+            exports.logErrorAndQuit(err, {state: 'saving', project: this.loadedProject});
+        }
+    }
+
+
+    projectLoad(code) {
+        if (!this.workspace) {
+            setTimeout(this.projectLoad.bind(this), 500, code);
+            return;
+        }
+
+        const xml = Blockly.Xml.textToDom(code.xml);
+        Blockly.Xml.domToWorkspace(xml, this.workspace);
+    }
+
+    /**
+     *
+     * @param workspace
+     */
+    onAttach(workspace) {
+        super.onAttach(workspace);
+
+        workspace.registerCodeObservable(this.codeObservable);
+        workspace.registerProjectSubscriber(this.projectLoad.bind(this));
+    }
+}
+
+module.exports = BlocklyComponent;
---
+++ desktop/workspace/components/code_component.js
@@ -0,0 +1,135 @@
+const {BaseComponent, TYPE_COMPONENT, TIMEOUT} = require('./component');
+const {ipcRenderer} = require('electron');
+const Config = require('electron-store');
+const electronConfig = new Config();
+const EDITOR_ID = 'editor';
+
+class CodeComponent extends BaseComponent {
+    static get ID() {
+        return 'codeComponent';
+    }
+
+    static get TITLE() {
+        return 'Code';
+    }
+
+    static registerComponent(workspace, ipc) {
+        workspace.layout.registerComponent(CodeComponent.ID, CodeComponent);
+        ipc.on('show_code', workspace.addComponentIfMissing.bind(workspace, CodeComponent.ID, CodeComponent.TITLE))
+    }
+
+    static generateContent(editorConfig = {}, subscriber = true) {
+        return {
+            type: TYPE_COMPONENT,
+            title: CodeComponent.TITLE,
+            componentName: CodeComponent.ID,
+            componentState: {
+                label: CodeComponent.ID,
+                editorOptions: editorConfig,
+                subscriber: subscriber
+            }
+        }
+    }
+
+    constructor(container, componentState) {
+        super(componentState);
+
+        this.codeContainer = container;
+        this.codeContainer.getElement().html('<div id="editor"></div>');
+
+        this.editorOptions = componentState.editorOptions || {};
+        this.subscriber = componentState.subscriber || true;
+
+        this.codeContainer.on('open', () => {
+            this.setupDOM();
+        });
+
+        this.codeContainer.on('resize', () => {
+            if (!this.editor) {
+                return;
+            }
+
+            this.editor.resize();
+        });
+
+        this.codeContainer.on('destroy', () => {
+            this.codeContainer = null;
+            this.editor = null;
+        });
+
+        this.codeSubscriber = {
+            next: code => {
+                if (code.code) {
+                    this.setCode(code.code);
+                }
+            }
+        }
+
+    }
+
+    setupDOM() {
+        if (!document.getElementById(EDITOR_ID)) {
+            setTimeout(() => {
+                this.setupDOM();
+            }, TIMEOUT);
+            return false;
+        }
+
+        document.getElementById(EDITOR_ID).style.fontSize = `${electronConfig.get('fontsize') || '12'}px`;
+        this.editor = ace.edit(EDITOR_ID);
+
+        this.editor.$blockScrolling = Infinity;
+
+        this.editor.getSession().setMode(this.editorOptions.editorLanguage);
+        this.editor.setReadOnly(this.editorOptions.readOnly);
+
+        let theme = electronConfig.get('theme');
+        if (theme) {
+            theme = theme.toLowerCase().replace(' ', '_');
+            this.editor.setTheme(`ace/theme/${theme}`);
+        }
+
+        return true;
+    }
+
+    setCode(code) {
+        if (!this.editor) {
+            setTimeout(this.setCode.bind(this), 500, code);
+            return;
+        }
+
+        this.editor.setValue(code, -1);
+    }
+
+    getCode() {
+        return this.editor.getValue();
+    }
+
+
+    updateSettings() {
+        let theme = electronConfig.get('theme');
+        if (theme && this.editor) {
+            theme = theme.toLowerCase().replace(' ', '_');
+            this.editor.setTheme(`ace/theme/${theme}`);
+        }
+
+        document.getElementById(EDITOR_ID).style.fontSize = `${electronConfig.get('fontsize') || '12'}px`
+    }
+
+    /**
+     * @override
+     * @param workspace
+     * @return {*}
+     */
+    onAttach(workspace) {
+        super.onAttach(workspace);
+
+        if (this.subscriber) {
+            workspace.registerCodeSubscriber(this.codeSubscriber);
+        }
+
+        ipcRenderer.on('settings_updated', this.updateSettings.bind(this));
+    }
+}
+
+module.exports = CodeComponent;
