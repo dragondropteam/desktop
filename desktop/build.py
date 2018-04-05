1fb7a37e13d02e03bfc2702ea6daa89e9fc58ceb
Luke Powell <raistlinpowell@gmail.com>
2018-04-05 13:10:01 -0700

    Force an error code if we are rate limited

--- desktop/build.py
+++ desktop/build.py
@@ -292,8 +292,7 @@ class Gen_compressed(threading.Thread):
             for error in errors:
                 print("SERVER ERROR: %s" % target_filename)
                 print(error["error"])
-            print('We dead')
-            sys.exit(1)
+            os._exit(1)
         elif json_data.has_key("errors"):
             errors = json_data["errors"]
             for error in errors:
