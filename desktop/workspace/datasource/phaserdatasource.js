616383be7081a9c154d7abf62807c73dbf6779be
Luke Powell <raistlinpowell@gmail.com>
2018-05-17 13:26:18 -0700

    Resolves DRAG-465

--- desktop/workspace/datasource/phaserdatasource.js
+++ desktop/workspace/datasource/phaserdatasource.js
@@ -4,26 +4,27 @@ const log = require('electron-log');
 
 class PhaserDataSource extends BlocklyDataSource {
 
-    /**
-     * First copy over all assets that are in this project, then simply call the existing save as to copy over blocks
-     * @param code The generated code to save out to destinationProject
-     * @param destinationProject The destination project to save the current project to
-     */
-    saveAs(code, destinationProject) {
-        try {
-            const assets = fs.readdirSync(this.project.getFileInProjectDir('assets'));
-            assets.forEach(file => {
-                fs.copySync(this.project.getFileInProjectDir('assets/' + file), destinationProject.getFileInProjectDir('assets/' + file));
-            });
-            super.saveAs(code, destinationProject);
-        } catch (err) {
-            if (err !== Error.ENOENT) {
-                throw err;
-            }
-        }
+  /**
+   * First copy over all assets that are in this project, then simply call the existing save as to copy over blocks
+   * @param code The generated code to save out to destinationProject
+   * @param destinationProject The destination project to save the current project to
+   */
+  saveAs (code, destinationProject) {
+    try {
+      const assets = fs.readdirSync(this.project.getFileInProjectDir('assets'));
+      assets.forEach(file => {
+        fs.copySync(this.project.getFileInProjectDir('assets/' + file), destinationProject.getFileInProjectDir('assets/' + file));
+      });
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
     }
 
-    // addAsset
+    super.saveAs(code, destinationProject);
+  }
+
+  // addAsset
 }
 
 module.exports = PhaserDataSource;
