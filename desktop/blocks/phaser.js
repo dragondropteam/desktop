638f028b33801385700b0ad6003d11e712672b39
Unknown <aeon.williams@digipen.edu>
2018-05-31 10:46:02 -0700

    Revert "Resolves DRAG-478"
    This reverts commit cbd6f4ab266d10030e49ca52ae8fec07c289013d.

--- desktop/blocks/phaser.js
+++ desktop/blocks/phaser.js
@@ -628,42 +628,6 @@ Blockly.Blocks['statemanager_start_state'] = {
     this.setHelpUrl(Blockly.Msg.STATEMANAGER_START_STATE_HELP_URL);
   }
 };
-
-Blockly.Blocks['statemanager_get_current_state'] = {
-  init: function() {
-    this.appendDummyInput()
-      .appendField(Blockly.Msg.STATEMANAGER_GET_CURRENT_STATE_FIELD_1);
-    this.setOutput(true, null);
-    this.setColour(PHASER_STATES_COLOUR);
-    this.setTooltip(Blockly.Msg.STATEMANAGER_GET_CURRENT_STATE_TOOLTIP);
-    this.setHelpUrl(Blockly.Msg.STATEMANAGER_GET_CURRENT_STATE_HELP_URL);
-  }
-};
-
-Blockly.Blocks['statemanager_restart_state'] = {
-  init: function() {
-    this.appendDummyInput()
-      .appendField(Blockly.Msg.STATEMANAGER_RESTART_STATE_FIELD_1);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(PHASER_STATES_COLOUR);
-    this.setTooltip(Blockly.Msg.STATEMANAGER_RESTART_STATE_TOOLTIP);
-    this.setHelpUrl(Blockly.Msg.STATEMANAGER_RESTART_STATE_HELP_URL);
-  }
-};
-
-Blockly.Blocks['statemanager_check_state'] = {
-  init: function() {
-    this.appendValueInput("NAME")
-      .setCheck("String")
-      .appendField(Blockly.Msg.STATEMANAGER_CHECK_STATE_FIELD_1);
-    this.setInputsInline(true);
-    this.setOutput(true, null);
-    this.setColour(PHASER_STATES_COLOUR);
-    this.setTooltip(Blockly.Msg.STATEMANAGER_CHECK_STATE_TOOLTIP);
-    this.setHelpUrl(Blockly.Msg.STATEMANAGER_CHECK_STATE_HELP_URL);
-  }
-};
 //endregion
 
 //region WORLD
